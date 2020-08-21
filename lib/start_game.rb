751fbcc5e6571d5c8811bf697c8099f13a44bfbc
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-21 09:10:04 -0500

    Refactor console output in StartGame class

--- lib/start_game.rb
+++ lib/start_game.rb
@@ -8,10 +8,9 @@ class StartGame
   end
 
   def start
-    #intro
-    #user_ready?
-    turn
-    # "*~*~*~* #{} has won the game! *~*~*~*"
+    intro
+    user_ready?
+    puts "\n\n*~*~*~* #{winner?} has won the game! *~*~*~*"
   end
 
 private
@@ -19,22 +18,24 @@ private
   def intro
     puts "\nWelcome to War! (or Peace) This game will be played with 52 cards.
     The players today are #{@player1.name} and #{@player2.name}.
-    Type 'GO' to start the game!
+    Type 'GO' to start the game! Press 'q' to quit.
     ------------------------------------------------------------------"
   end
 
   def user_ready?
     user_input = gets.chomp
-
-    if user_input.upcase == 'GO'
-      nil
-    else
-      puts "Type 'GO' if you're ready!"
+    
+    until user_input.upcase == 'GO' || user_input.downcase == 'q'
+      puts "Type 'GO' if you're ready! Press 'q' to quit."
+      user_input = gets.chomp
     end
+
+    return turn if user_input.upcase == 'GO'
+    return nil if user_input.downcase == 'q'
   end
 
   def turn
-    until player_lost? || @turn_number == 50_000
+    until player_lost? || @turn_number == 1_000_000
       turn = Turn.new(@player1, @player2)      
       @turn_number += 1
       turn.pile_cards
@@ -46,6 +47,11 @@ private
 
   def player_lost?
     @player1.has_lost? || @player2.has_lost?
+  end
+
+  def winner?
+    return @player2.name if @player1.has_lost?
+    return @player1.name if @player2.has_lost?
   end
 
 end
