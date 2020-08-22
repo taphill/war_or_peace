15c1da05dc1dcf298fd84882d79b571172425fd3
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-22 16:40:57 -0500

    Add create_card test

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/card'
 require './lib/card_generator'
 
 class CardGeneratorTest < Minitest::Test
@@ -17,5 +18,14 @@ class CardGeneratorTest < Minitest::Test
     deck = CardGenerator.new(filename)
   
     assert_equal [], deck.cards
+  end
+
+  def test_it_can_create_a_card
+    filename = "cards.txt"
+
+    deck = CardGenerator.new(filename)
+    deck.create_cards
+
+    assert_instance_of Card, deck.cards[0]
   end
 end
