379adc8803298f0fdbf09ede67e7e185569b0c59
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-23 12:56:10 -0500

    Refactor runner file

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/start_game'
 require './lib/card_generator'
 
-filename = "cards.txt"
+filename = 'cards.txt'
 
 deck = CardGenerator.new(filename)
 deck.create_cards
 
 shuffled_deck = deck.cards.shuffle
 
-deck_1 = Deck.new(shuffled_deck.slice(0..25))
-deck_2 = Deck.new(shuffled_deck.slice(26..51))
+deck1 = Deck.new(shuffled_deck.slice(0..25))
+deck2 = Deck.new(shuffled_deck.slice(26..51))
 
-player1 = Player.new("Megan", deck_1)
-player2 = Player.new("Aurora", deck_2)
+player1 = Player.new('Megan', deck1)
+player2 = Player.new('Aurora', deck2)
 
 start_game = StartGame.new(player1, player2)
 start_game.start
